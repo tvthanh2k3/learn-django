5e82eb2fc146812b3d600ce6d5abfc1b5f64a562
tvthanh2k3 <tvthanh1201@gmail.com>
2025-08-13 14:31:55 +0700

    feat: foreign keys

--- leads/models.py
+++ leads/models.py
@@ -5,3 +5,9 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+
+class Agent(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
